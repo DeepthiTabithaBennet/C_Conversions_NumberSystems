337a3d7c10dfd591c12b1336b4d8c8cc17cc2338
Deepthi Tabitha Bennet <71541429+DeepthiTabithaBennet@users.noreply.github.com>
2021-04-27 17:38:00 +0530

    Update Decimal_to_Binary.c

--- Decimal_to_Binary.c
+++ Decimal_to_Binary.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 
 long decimal_to_Binary(int decimalnum){
-
 	long binarynum = 0;
 	int mod, place = 1;
-	
 
 	while (decimalnum != 0){
-	
 		mod = decimalnum % 2;
 		decimalnum = decimalnum / 2;
 		binarynum = binarynum + (mod * place);
@@ -16,10 +13,7 @@ long decimal_to_Binary(int decimalnum){
 	return binarynum;
 }
 
-
-
 int main(){
-
 	int decimalnum;
 	scanf("Enter a Decimal number : %d", &decimalnum);
 	printf("Equivalent Binary Number is: %ld", decimal_to_Binary(decimalnum));
